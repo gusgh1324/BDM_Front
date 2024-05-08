d5109ed90a06501fc04489416a26f4d054a8e979
DESKTOP-1N5612G\it <gusgh1324@naver.com>
2024-05-08 11:26:52 +0900

    헤더 Link로 정리

--- main/src/components/layout/Header.js
+++ main/src/components/layout/Header.js
@@ -11,9 +11,9 @@ const Header = () => {
     <div className="bg-white">
       <nav className="nav-container">
         <div className="logo-container">
-          <a className="text-2xl font-bold" href="/">
+          <Link to={"/"}>
             <img alt="Logo" className="h-32" src="/logo512.png" />
-          </a>
+          </Link>
           <div className="nav-links">
             <div className="nav-link">
               <Link to={"/disease"}>질병정보</Link>
@@ -45,23 +45,28 @@ const Header = () => {
           </button>
         </div>
       </nav>
-      {isOpen && (
-        <div className="md:hidden">
-          <a className="mobile-nav-link" href="#">
-            질병정보
-          </a>
-          <a className="mobile-nav-link" href="#">
-            도구 및 API
-          </a>
-          <a className="mobile-nav-link" href="#">
-            구독
-          </a>
-          <div className="px-4 py-4">
-            <Button className="mobile-login-button">로그인</Button>
-            <Button className="mobile-signup-button">회원가입</Button>
+      <section className="mobile">
+        {isOpen && (
+          <div className="md:hidden">
+            <div className="nav-link">
+              <Link to={"/disease"}>질병정보</Link>
+            </div>
+
+            <div className="nav-link">
+              <Link to={"/"}>도구 및 API</Link>
+            </div>
+
+            <div className="nav-link">
+              <Link to={"/"}>구독</Link>
+            </div>
+
+            <div className="px-4 py-4">
+              <Button className="mobile-login-button">로그인</Button>
+              <Button className="mobile-signup-button">회원가입</Button>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </section>
     </div>
   );
 };
