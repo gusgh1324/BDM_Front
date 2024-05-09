3c86aab70380c09fbc1a077feaa0d3d6634e838c
slicepizza <tmdgns8399@naver.com>
2024-05-10 00:04:10 +0900

    맵 코드 가독성 최적화

--- main/src/page/Map.tsx
+++ main/src/page/Map.tsx
@@ -13,38 +13,22 @@ const Map = () => {
   useEffect(() => {
     let container = document.getElementById("map");
     let options = {
-      center: new window.kakao.maps.LatLng(35.1, 129.05),
-      level: 7,
+      center: new window.kakao.maps.LatLng(35.12, 129.06),
+      level: 8,
     };
-
     let map = new window.kakao.maps.Map(container, options);
 
-    let centerPosition = new window.kakao.maps.LatLng(35.079338, 129.0756457);
-    let centerMarker = new window.kakao.maps.Marker({
-      position: centerPosition,
-    });
-    centerMarker.setMap(map);
-
-    let centerOverlayContent = `
-    <div class="custom-overlay">
-      <div class="custom-overlay-title">국립수산물품질관리원</div>
-      <div class="custom-overlay-content">부산 영도구 해양로 337</div>
-    </div>
-  `;
-    let centerOverlay = new window.kakao.maps.CustomOverlay({
-      map: map,
-      position: centerPosition,
-      content: centerOverlayContent,
-      yAnchor: 1.75,
-    });
-    centerOverlay.setMap(map);
-
     const locations = [
-      {
-        title: "국립수산물품질관리원 부산지원",
-        latlng: new window.kakao.maps.LatLng(35.099802, 129.0373695),
-        content: "부산 중구 중앙대로30번길 8",
-      },
+      // {
+      //   title: "국립수산물품질관리원",
+      //   latlng: new window.kakao.maps.LatLng(35.079338, 129.0756457),
+      //   content: "부산 영도구 해양로 337",
+      // },
+      // {
+      //   title: "국립수산물품질관리원 부산지원",
+      //   latlng: new window.kakao.maps.LatLng(35.099802, 129.0373695),
+      //   content: "부산 중구 중앙대로30번길 8",
+      // },
       {
         title: "부경대학교 수산질병관리원",
         latlng: new window.kakao.maps.LatLng(35.1337774, 129.10865),
