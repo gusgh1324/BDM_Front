dadd21293e75a55e13a41ecedd21d91538908462
slicepizza <tmdgns8399@naver.com>
2024-05-11 11:15:22 +0900

    탬플릿 활용 userprofile 구현

--- main/src/components/ui/AuthButtons.tsx
+++ main/src/components/ui/AuthButtons.tsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
 import { Button } from "./button";
+import React from "react";
 
 const AuthButtons = () => {
   return (
-    <>
+    <React.Fragment>
       <Link to={"/login"}>
         <Button className="login-button">로그인</Button>
       </Link>
       <Link to={"/register"}>
         <Button className="register-button">회원가입</Button>
       </Link>
-    </>
+    </React.Fragment>
   );
 };
 
--- main/src/components/ui/UserProfile.tsx
+++ main/src/components/ui/UserProfile.tsx
@@ -1,30 +1,28 @@
 import React, { Fragment } from "react";
-import { Menu, Transition } from "@headlessui/react";
-import { BellIcon } from "@heroicons/react/24/outline";
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Transition,
+  MenuSeparator,
+} from "@headlessui/react";
+import "../layout/Header.css";
 
 const UserProfile = () => {
   return (
-    <>
-      <button
-        type="button"
-        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-      >
-        <span className="absolute -inset-1.5" />
-        <span className="sr-only">View notifications</span>
-        <BellIcon className="h-6 w-6" aria-hidden="true" />
-      </button>
-
+    <React.Fragment>
       <Menu as="div" className="relative ml-3">
         <div>
-          <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+          <MenuButton className="menu-button">
             <span className="absolute -inset-1.5" />
             <span className="sr-only">Open user menu</span>
             <img
-              className="h-8 w-8 rounded-full"
+              className="h-12 w-12 rounded-full"
               src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
               alt=""
             />
-          </Menu.Button>
+          </MenuButton>
         </div>
         <Transition
           as={Fragment}
@@ -35,47 +33,36 @@ const UserProfile = () => {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={`${
-                    active ? "bg-gray-100" : ""
-                  } block px-4 py-2 text-sm text-gray-700`}
-                >
-                  Your Profile
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={`${
-                    active ? "bg-gray-100" : ""
-                  } block px-4 py-2 text-sm text-gray-700`}
-                >
-                  Settings
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={`${
-                    active ? "bg-gray-100" : ""
-                  } block px-4 py-2 text-sm text-gray-700`}
-                >
-                  Sign out
-                </a>
-              )}
-            </Menu.Item>
-          </Menu.Items>
+          <MenuItems className="menu-items">
+            <MenuItem>
+              {({ focus }) => {
+                const itemClassName = `${
+                  focus ? "bg-gray-100" : ""
+                } block w-full rounded-md px-3 py-2 text-left text-sm text-gray-700`;
+                return <button className={itemClassName}>마이페이지</button>;
+              }}
+            </MenuItem>
+            <MenuItem>
+              {({ focus }) => {
+                const itemClassName = `${
+                  focus ? "bg-gray-100" : ""
+                } block w-full rounded-md px-3 py-2 text-left text-sm text-gray-700`;
+                return <button className={itemClassName}>설정</button>;
+              }}
+            </MenuItem>
+            <MenuSeparator className="mx-1 my-1 h-px bg-gray-200" />
+            <MenuItem>
+              {({ focus }) => {
+                const itemClassName = `${
+                  focus ? "bg-gray-100 text-red-600" : "text-red-600"
+                } block w-full rounded-md px-3 py-2 text-left text-sm text-gray-700`;
+                return <button className={itemClassName}>로그아웃</button>;
+              }}
+            </MenuItem>
+          </MenuItems>
         </Transition>
       </Menu>
-    </>
+    </React.Fragment>
   );
 };
 
