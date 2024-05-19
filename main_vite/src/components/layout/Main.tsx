df978229840c2ba6b92c5eed35889d1d361c4a10
slicepizza <tmdgns8399@naver.com>
2024-05-20 00:29:40 +0900

    메인페이지 애니메이션 적용

--- main_vite/src/components/layout/Main.tsx
+++ main_vite/src/components/layout/Main.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import CloudUploadIcon from "../icon/CloudUploadIcon";
 import "./Main.css";
 import { useImageStore } from "../../Store";
@@ -5,6 +6,8 @@ import { useImageStore } from "../../Store";
 const Main = () => {
   const { uploadedImage, analysisResult, setUploadedImage, setAnalysisResult } =
     useImageStore();
+  const [animate, setAnimate] = useState(false);
+  const [newTextBoxState, setNewTextBoxState] = useState(false);
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -16,14 +19,30 @@ const Main = () => {
         // ...
         // 분석 결과를 analysisResult 상태에 저장
         setAnalysisResult("분석 결과 예시");
+        setAnimate(true); // 애니메이션 트리거
       };
       reader.readAsDataURL(file);
     }
   };
 
+  useEffect(() => {
+    if (animate) {
+      const timeout = setTimeout(() => {
+        setNewTextBoxState(true);
+      }, 100); // 기존 텍스트 박스가 사라진 후 0.1초 딜레이로 애니메이션 시작
+      return () => clearTimeout(timeout);
+    }
+  }, [animate]);
+
   return (
-    <div className="flex flex-col mt-10 md:flex-row">
-      <div className={`md:w-1/2 md:pr-16 ${uploadedImage ? "hidden" : ""}`}>
+    <div className="relative flex flex-col mt-10 md:flex-row">
+      <div
+        className={`transition-all duration-1000 overflow-hidden max-h-[70vh] ${
+          uploadedImage
+            ? "opacity-0 md:w-0 md:pr-0"
+            : "opacity-100 md:w-1/2 md:pr-16"
+        }`}
+      >
         <h1 className="main-title">
           생선의 상태를 <br /> 분석하세요
         </h1>
@@ -37,7 +56,7 @@ const Main = () => {
           업로드하는 것만으로도 내부적인 질병까지 정확하게 진단받을 수 있습니다.
         </p>
       </div>
-      <div className={`mt-8 md:w-1/2 ${uploadedImage ? "" : "md:order-last"}`}>
+      <div className="mt-8 md:w-1/2 flex justify-center">
         <label className="upload-label" htmlFor="file-upload">
           <div className="relative w-full h-full">
             {uploadedImage ? (
@@ -63,10 +82,19 @@ const Main = () => {
           />
         </label>
       </div>
-      <div className={`md:w-1/2 md:pl-4 ${analysisResult ? "" : "hidden"}`}>
-        <h2 className="main-title">분석 결과</h2>
-        <p className="main-description">{analysisResult}</p>
-      </div>
+      {uploadedImage && (
+        <div
+          className={`absolute transition-all duration-1000 delay-1000 overflow-hidden max-h-[70vh] ${
+            newTextBoxState
+              ? "opacity-100 w-full md:w-1/2 translate-x-0"
+              : "opacity-0 w-0 translate-x-full"
+          } md:pl-4`}
+          style={{ right: 0 }}
+        >
+          <h2 className="main-title">분석 결과</h2>
+          <p className="main-description">{analysisResult}</p>
+        </div>
+      )}
     </div>
   );
 };
