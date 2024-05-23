fc180cfde16cc7777cb8476d9885daec74e00f07
gloryshin <gloryshin@gmail.com>
2024-05-23 14:02:14 +0900

    buttonedit

--- main_vite/src/components/layout/mypage/MyAccountLatout.tsx
+++ main_vite/src/components/layout/mypage/MyAccountLatout.tsx
@@ -12,7 +12,7 @@ const MyComponent = () => {
       </p>
       <Link
         to=""
-        className="w-full max-w-xs px-4 py-2 font-bold text-white bg-pink-300 rounded hover:bg-pink-400"
+        className="w-full max-w-xs px-4 py-2 font-bold text-white bg-pink-300 rounded hover:bg-pink-400r"
       >
         Delete Account
       </Link>
